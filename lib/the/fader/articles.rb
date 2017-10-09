a91be6c21a5aa51a867de5171247b944d82bd102
Ryan Lonergan <ryan.tlonergan@gmail.com>
2017-10-09 14:33:30 -0400

    initializing repo for CLI project

---
+++ lib/the/fader/articles.rb
@@ -0,0 +1,9 @@
+require "the/fader/articles/version"
+
+module The
+  module Fader
+    module Articles
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/the/fader/articles/version.rb
@@ -0,0 +1,7 @@
+module The
+  module Fader
+    module Articles
+      VERSION = "0.1.0"
+    end
+  end
+end
